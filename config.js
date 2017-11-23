12d3c5de0a8c328e2dc735a4de7e5a4774e136b0
cmdevendran <cmdevendran@gmail.com>
2017-11-23 15:55:30 +0800

    s3 upload

--- config.js
+++ config.js
@@ -5,6 +5,9 @@ module.exports = {
   "typ": "JWT"
 },
     'secret': 'sgrestaurant@1234S',
-    'db' : 'mongodb://user:password@ds161262.mlab.com:61262/sgrestaurant'
+    'db' : 'mongodb://user:password@ds161262.mlab.com:61262/sgrestaurant',
+    'AWSAccessKeyId':'AKIAICNVBDPKUXND6XNA',
+    'AWSSecretKey':'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',
+    'storage':'sgrestaurant'
 
 };

ed3d129e6db79be66b4860a8b0900af7c5fdd17b
cmdevendran <cmdevendran@gmail.com>
2017-11-24 18:14:15 +0800

    removed the aws public key

--- config.js
+++ config.js
@@ -5,9 +5,7 @@ module.exports = {
   "typ": "JWT"
 },
     'secret': 'sgrestaurant@1234S',
-    'db' : 'mongodb://user:password@ds161262.mlab.com:61262/sgrestaurant',
-    'AWSAccessKeyId':'AKIAICNVBDPKUXND6XNA',
-    'AWSSecretKey':'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',
+  
     'storage':'sgrestaurant'
 
 };
